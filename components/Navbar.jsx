e218695f43a883fae4433634d00cca5572894c5d
AshrafOhi <ashrafohi20@gmail.com>
2025-11-04 09:27:25 +0600

    navbar-added

---
+++ components/Navbar.jsx
@@ -0,0 +1,102 @@
+"use client";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useRef } from "react";
+
+const Navbar = () => {
+  const mobileMenuRef = useRef();
+
+  const openMenu = () => {
+    mobileMenuRef.current.style.transform = "translate(-16rem)";
+  };
+
+  const closeMenu = () => {
+    mobileMenuRef.current.style.transform = "translate(16rem)";
+  };
+
+  return (
+    <>
+      {/* background image */}
+
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} className="w-full" alt="bg-image" />
+      </div>
+
+      {/* nav starts */}
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] flex items-center justify-between z-50">
+        <a href="#top">
+          <Image
+            src={assets.logo}
+            alt="logo"
+            className="w-24 cursor-pointer mr-14"
+          />
+        </a>
+
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+          <li>
+            <a href="#top">Home</a>
+          </li>
+          <li>
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+        </ul>
+
+        {/* buttons */}
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} alt="theme-button" className="w-6" />
+          </button>
+
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-grey-500 rounded-full ml-4"
+          >
+            Contact{" "}
+            <Image src={assets.arrow_icon} alt="contact" className="w-3" />
+          </a>
+
+          <button className="block md:hidden ml-4" onClick={openMenu}>
+            <Image src={assets.menu_black} alt="menu" className="w-6" />
+          </button>
+        </div>
+
+        {/* mobile menu */}
+        <ul
+          ref={mobileMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          {/* close menu */}
+
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt="close-menu"
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li onClick={closeMenu}>
+            <a href="#top">Home</a>
+          </li>
+          <li onClick={closeMenu}>
+            <a href="#about">About me</a>
+          </li>
+          <li onClick={closeMenu}>
+            <a href="#projects">Projects</a>
+          </li>
+          <li onClick={closeMenu}>
+            <a href="#experience">Experience</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
